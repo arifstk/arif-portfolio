39b1659a132f934ab6fa910c6d65ba4ac8f3966e
arifstk <arifsk.dev@gmail.com>
2026-06-03 23:26:17 +0600

    dark_light

---
+++ components/Helper/Home/Home.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button'
+import ThemeToggler from '../ThemeToggler'
+
+const Home = () => {
+  return (
+    <div className='overflow-hidden'>
+      <div>Home Page</div>
+      <ThemeToggler />
+    </div>
+  )
+}
+
+export default Home
+

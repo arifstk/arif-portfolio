fc791ab6875e5ace86aa468fe1b91d35ad9ad4ea
arifstk <arifsk.dev@gmail.com>
2026-07-18 16:18:51 +0600

    skill pg ui

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -27,7 +27,7 @@ export default function Skills() {
             </h3>
 
             {/* Skills Badges/Cards Grid */}
-            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 sm:gap-8">
+            <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-7 gap-4 sm:gap-8">
               {category.skills.map((skill, skillIndex) => {
                 // Instantiating the dynamic icon component assigned from constants
                 const IconComponent = skill.icon;
@@ -35,10 +35,10 @@ export default function Skills() {
                 return (
                   <div
                     key={skillIndex}
-                    className="group flex flex-col items-center justify-center p-5 bg-linear-to-br from-violet-100 via-purple-50 to-indigo-50 dark:from-violet-950/40 dark:via-purple-950/20 dark:to-slate-900  dark:border-gray-700 rounded-xl border border-black/10 transition-all duration-300 hover:-translate-y-1 hover:shadow-[0_10px_25px_rgba(0,123,255,0.06)] hover:border-[#007bff]/20"
+                    className="group flex flex-col items-center justify-center p-3 sm:p-5 bg-linear-to-br from-violet-100 via-purple-50 to-indigo-50 dark:from-violet-950/40 dark:via-purple-950/20 dark:to-slate-900  dark:border-gray-700 rounded-xl border border-black/10 transition-all duration-300 hover:-translate-y-1 hover:shadow-[0_10px_25px_rgba(0,123,255,0.06)] hover:border-[#007bff]/20"
                   >
                     {/* Icon Wrapper with Hover Animation */}
-                    <div className="p-3 bg-white dark:bg-gray-700 dark:border-gray-400 rounded-lg text-violet-700 transition-all duration-300 group-hover:text-[#2b7473] group-hover:scale-110 shadow-[0_2px_8px_rgba(0,0,0,0.02)] group-hover:shadow-[0_4px_12px_rgba(0,123,255,0.1)] mb-3">
+                    <div className="p-1.5 sm:p-3 bg-white dark:bg-gray-700 dark:border-gray-400 rounded-lg text-violet-700 transition-all duration-300 group-hover:text-[#2b7473] group-hover:scale-110 shadow-[0_2px_8px_rgba(0,0,0,0.02)] group-hover:shadow-[0_4px_12px_rgba(0,123,255,0.1)] mb-3">
                       {IconComponent && <IconComponent className="w-6 h-6 stroke-[1.75]" />}
                     </div>
 

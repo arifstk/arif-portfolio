33c6cc3d6e11f741f3346d4aef776e9b0e9dc7b8
arifstk <arifsk.dev@gmail.com>
2026-07-18 23:07:51 +0600

    pulse up arrow footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -58,7 +58,7 @@ export default function Footer() {
         >
           Back to top
           <div className="p-1.5 rounded-lg bg-white dark:bg-slate-800 border border-slate-100 dark:border-slate-500 group-hover:border-[#007bff]/20 group-hover:bg-[#007bff]/5 transition-all duration-300">
-            <ArrowUp className="w-3.5 h-3.5 group-hover:text-violet-600 group-hover:-translate-y-1 transition-all duration-300" />
+            <ArrowUp className="w-3.5 h-3.5 group-hover:text-violet-600 text-violet-700 animate-pulse group-hover:-translate-y-1 transition-all duration-300" />
           </div>
         </button>
       </div>

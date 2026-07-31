25d81d46ecb9860d57ff4362c07417e7682a42d2
arifstk <arifsk.dev@gmail.com>
2026-08-01 01:17:00 +0600

    production

--- lib/emailTemplate.ts
+++ lib/emailTemplate.ts
@@ -6,7 +6,7 @@ export function getOfferEmailTemplate(
 ) {
   return `
   <!DOCTYPE html>
-  <html>
+  <html> 
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">

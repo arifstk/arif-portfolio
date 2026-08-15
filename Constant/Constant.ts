4843e22f58bcb3b2bd0016f52d3cfaa048051756
arifstk <arifsk.dev@gmail.com>
2026-08-16 00:24:57 +0600

    robot.ts useragent

--- Constant/Constant.ts
+++ Constant/Constant.ts
@@ -1,22 +1,18 @@
 // Constant/Constant.ts
 
-// import { Mail, MapPin, Phone } from "lucide-react";
 import { Cloud, Code2, Database, GitBranch, Globe, Layers, Mail, MapPin, Palette, Phone, Server, Smartphone, Terminal } from "lucide-react";
 import { FiGithub } from "react-icons/fi";
 import { FaStripe } from "react-icons/fa6";
 import { SiExpress, SiMongoosedotws } from "react-icons/si";
 import { LuWebhook } from "react-icons/lu";
-import { FaNodeJs, FaReact, FaDocker, FaFigma } from "react-icons/fa";
-import { RiNextjsLine, RiTailwindCssFill } from "react-icons/ri";
-import { TbBrandMongodb, TbBrandSocketIo, TbBrandTypescript } from "react-icons/tb";
+import { FaNodeJs, FaReact, FaFigma } from "react-icons/fa";
+import { TbBrandMongodb, TbBrandSocketIo, TbBrandTypescript, TbBrandNextjs } from "react-icons/tb";
+import { BiLogoTailwindCss } from "react-icons/bi";
 
 export const NavLinks = [
-  // {name: 'Home', path: '/'},
-  // {name: 'Skills', path: '/skills'},
   {name: 'Work', path: '/projects'},
   {name: 'Blog', path: '/blog'},
   {name: 'About', path: '/about'},
-  // {name: 'Contact', path: '/contact'},
 ];
 
 export const statsData = [
@@ -52,10 +48,9 @@ export const skillCategories = [
     title: "Frontend",
     skills: [
       { name: "React", icon: FaReact },
-      { name: "Next.js", icon: RiNextjsLine },
+      { name: "Next.js", icon: TbBrandNextjs },
       { name: "TypeScript", icon: TbBrandTypescript },
-      { name: "Tailwind CSS", icon: RiTailwindCssFill },
-      // { name: "React Native", icon: Smartphone },
+      { name: "Tailwind CSS", icon: BiLogoTailwindCss },
     ],
   },
   {
@@ -64,8 +59,6 @@ export const skillCategories = [
       { name: "Node.js", icon: FaNodeJs },
       { name: "Express", icon: SiExpress },
       { name: "MongoDB", icon: TbBrandMongodb },
-      // { name: "PostgreSQL", icon: Database },
-      // { name: "GraphQL", icon: Cpu },
       { name: "REST APIs", icon: Cloud },
       { name: "Mongoose", icon: SiMongoosedotws },
       { name: "Socket io", icon: TbBrandSocketIo },
@@ -76,13 +69,9 @@ export const skillCategories = [
     skills: [
       { name: "Git", icon: GitBranch },
       { name: "Github", icon: FiGithub },
-      // { name: "Docker", icon: FaDocker },
-      // { name: "AWS", icon: Cloud },
-      // { name: "Linux", icon: Terminal },
       { name: "Figma", icon: FaFigma },
       { name: "Stripe", icon: FaStripe },
       {name: "Webhooks", icon: LuWebhook },
-      // { name: "CI/CD", icon: Cpu },
     ],
   },
 ];
--- app/robots.ts
+++ app/robots.ts
@@ -6,7 +6,15 @@ import { MetadataRoute } from "next";
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
-       {
+      {
+        userAgent: "Google-InspectionTool",
+        allow: "/",
+      },
+      {
+        userAgent: "Chrome-Lighthouse",
+        allow: "/",
+      },
+      {
         userAgent: "*",
         allow: [
           "/",

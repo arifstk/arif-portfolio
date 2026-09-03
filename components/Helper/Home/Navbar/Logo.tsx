efbb93cd157103a98a477703d4f50d5b6225b055
Shaikh Arif Hossain <arifsk.dev@gmail.com>
2026-09-03 09:37:10 +0600

    Remove responsive width and height from logo SVG

--- components/Helper/Home/Navbar/Logo.tsx
+++ components/Helper/Home/Navbar/Logo.tsx
@@ -40,7 +40,7 @@ export default function Logo() {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 380 320"
-          className="w-12 h-10 sm:w-14 sm:h-12"
+          className="w-12 h-10"
           role="img"
           aria-label="SA logo"
         >
